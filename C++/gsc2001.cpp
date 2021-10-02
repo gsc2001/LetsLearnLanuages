a8023815e9ee7d8b5bdfaa2d13f35cdd95a74a29
Gurkirat Singh <gurkiratsingh2001@gmail.com>
2021-10-02 19:33:16 +0530

    C++ code

---
+++ C++/gsc2001.cpp
@@ -0,0 +1,15 @@
+/*
+     Author:Gurkirat Singh
+     Written on:"2021-10-02"
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Gurkirat Singh\n";
+    string str;
+    cin >> str;
+    cout << str.size() << "\n";
+    return 0;
+}
